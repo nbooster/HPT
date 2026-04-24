c09f6336438cfe51374b5ceb508525073766b328
Panos <notisworld@hotmail.gr>
2026-04-25 02:41:49 +0300

    Update HPT.hpp

--- HPT.hpp
+++ HPT.hpp
@@ -438,7 +438,7 @@ public:
 
 			os << 
 			"\n\n*To get the measurements also in nanoseconds,\n please provide the positive 'cpuSpeedInMGHz' argument in 'PrintResults' (assuming CPU has invariant TSC support)."
-			"\n\n**To find the actuall CPU speed of a core, run this on Linux: watch -n.1 \"grep \"^[c]pu MHz\" /proc/cpuinfo\""
+			"\n\n**To find the actuall CPU speed of a core, run this on Linux: watch -n.1 \"grep \\\"^[c]pu MHz\\\" /proc/cpuinfo\""
 			"\n\n***For more accurate results: "
 			"\n  do not perform nested measurements,"
 			"\n  measure only from one thread,"
